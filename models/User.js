26b571e3f6b11279cccae037413e22dd2497235a
elexis <eebrown422@gmail.com>
2022-03-03 15:36:27 -0500

    z

--- models/User.js
+++ models/User.js
@@ -1,19 +1,35 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email : {
          type: String,
-         required: true,
+         required: [true, 'Please enter an email'],
          unique: true,
-         lowercase: true
+         lowercase: true,
+         validate: [ isEmail, 'Please enter a valid email']
     },
     password: {
 
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password.'],
+        minLength:[ 6, 'Minimum password length is 6 characters']
     },
 });
+//fire a function after doc saved to db 
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc)
+     next();
+});
+
+///fire a function before doc saves to db
+
+userSchema.pre('save', function (next) {
+    console.log('user about to be created & saved', this);  
+    next();
+})
 
 const User = mongoose.model('user', userSchema);
 
